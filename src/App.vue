f7ec7f6c14f69920c96308a3434c71af764649d7
k3y6reak <k3y6reak@k3y6reak.local>
2019-08-07 16:32:03 +0900

    [+] Add AppSideMenuBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,21 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div>
+    <v-app dark>
+      <v-content>
+        <router-view />
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
